488b55bccb994b6f9e25489e21110c5c6b4b31b0
Jalindar <jalindarjk12@gmail.com>
2024-03-10 22:55:55 +0530

    added

--- loop.py
+++ loop.py
@@ -1,1 +1,57 @@
-print("i am here in loop");
+#for x in range(2,33,3):
+ # print(x)
+#else:
+#  print("Finally finished");  
+
+#Note: The else block will NOT be executed if the loop is stopped by a break statement.
+#for x in range(6):
+ # if x == 3: break
+  #print(x)
+#else:
+ # print("Finally finished!")
+
+#for x in [0, 5]:
+#  pass
+
+#for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error.
+  
+'''
+def myfun():
+    return print("in fun");
+print(myfun());
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil")
+'''
+
+
+#cars = ["Ford", "Volvo", "BMW"]
+#cars.append("Porshe");
+#cars.clear();
+#xcars = cars.copy();
+#cars.extend("Hero");  # ['Ford', 'Volvo', 'BMW', 'Porshe', 'H', 'e', 'r', 'o']
+#cars.pop()
+#cars.remove();
+#cars.sort()
+#print(cars);
+
+'''
+def myFunc(e):
+  return e['year']
+
+cars = [
+  {'car': 'Ford', 'year': 2005},
+  {'car': 'Mitsubishi', 'year': 2000},
+  {'car': 'BMW', 'year': 2019},
+  {'car': 'VW', 'year': 2011}
+]
+
+cars.sort(key=myFunc)
+print(cars)
+'''
+
+
+
+
